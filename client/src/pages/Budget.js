c9a20cf7cbe1017cbd578702b40cffa70eb981c2
willhitch <hitch.will@gmail.com>
2020-08-22 10:56:41 -0700

    added new pages and removed routes

---
+++ client/src/pages/Budget.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Budget = () => {
+  return <p>Hello Budget!</p>;
+};
+
+export default Budget;
---
+++ client/src/pages/Header.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Header = () => {
+  return <p>Hello Header!</p>;
+};
+
+export default Header;
