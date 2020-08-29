87fc668bdc6955146d1d22f89bf1e121d6fa0068
willhitch <hitch.will@gmail.com>
2020-08-29 09:23:25 -0700

    added trip item component

--- client/src/components/TripItem.js
+++ client/src/components/TripItem.js
@@ -2,7 +2,9 @@ import React from 'react'
 import { motion } from 'framer-motion'
 
 const TripItem = () => {
-
+    return(
+        <a href="#!" title="View Chart" className="collection-item">{name}<span style={{float: "right"}}><i className="material-icons" >show_chart</i></span></a>
+    )
 }
 
 export default TripItem
