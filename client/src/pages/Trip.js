703f59a07bf99f1c58563b31938ef8b16cf0b380
willhitch <hitch.will@gmail.com>
2020-08-29 13:08:28 -0700

    added card functionality

--- client/src/pages/Trip.js
+++ client/src/pages/Trip.js
@@ -1,56 +1,66 @@
-import React from "react";
-import Budget from "../components/Budget";
+import React, { useState } from "react";
+import TripItem from "../components/TripItem";
+import TripDetails from "../components/TripDetails";
+import projectData from "../data/projects.json";
 
 const Trip = () => {
+  const [tripInfo, setTripInfo] = useState({
+    trips: projectData,
+    selectedTrip: null,
+  });
+
+  const { selectedTrip, trips } = tripInfo;
+
   return (
     <div className="container">
       <br />
       <br />
       <div className="row">
-        <ul
-          className="sidenav sidenav-fixed collection with-header"
-          style={{
-            zIndex: "0",
-            height: "auto",
-            marginTop: "64px",
-            paddingBottom: "0",
-          }}
-        >
-          <li className="collection-header">
-            <h4>
-              Trips
-              <span style={{ float: "right" }}>
-                <i className="material-icons" style={{ fontSize: "3rem" }}>
-                  landscape
-                </i>
-              </span>
-            </h4>
-          </li>
-          <a href="#!" title="View Chart" className="collection-item">
-            Australia
-            <span style={{ float: "right" }}>
-              <i className="material-icons">show_chart</i>
-            </span>
-          </a>
-          <a href="#!" title="View Chart" className="collection-item">
-            Poland
-            <span style={{ float: "right" }}>
-              <i className="material-icons">show_chart</i>
-            </span>
-          </a>
-          <a href="#!" title="View Chart" className="collection-item">
-            Costa Rica
-            <span style={{ float: "right" }}>
-              <i className="material-icons">show_chart</i>
-            </span>
-          </a>
-          <a href="#!" title="View Chart" className="collection-item">
-            India
-            <span style={{ float: "right" }}>
-              <i className="material-icons">show_chart</i>
-            </span>
-          </a>
-        </ul>
+        <div class="col s6 m4">
+          <ul
+            className="sidenav sidenav-fixed collection with-header"
+            style={{
+              zIndex: "0",
+              height: "300px",
+              marginTop: "85px",
+              marginLeft: "20px",
+              paddingBottom: "0",
+            }}
+          >
+            <li className="collection-header">
+              <h4>
+                Trips
+                <span style={{ float: "right" }}>
+                  <i className="material-icons" style={{ fontSize: "3rem" }}>
+                    landscape
+                  </i>
+                </span>
+              </h4>
+            </li>
+            {trips ? (
+              trips.map((trip) => (
+              <TripItem
+                key={trip.id}
+                location={trip.name}
+                isSelected={trip === selectedTrip}
+                selectTrip={() => setTripInfo({ ...trips, selectedTrip: trip })}
+              />
+            ))) : (<p>Create a Trip</p>)}
+            
+          </ul>
+        </div>
+        <div class="col s6 m6">
+          {selectedTrip ? (
+            <TripDetails
+              location={selectedTrip.name}
+              details={selectedTrip.description}
+              image={selectedTrip.screenshot}
+              link={selectedTrip.github}
+            />
+          ) : (
+            <h3>Click on a trip to see details</h3>
+          )}
+        </div>
       </div>
     </div>
   );
