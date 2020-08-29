2f287d4d4c943ee7dd3704a343b796162337a6e6
willhitch <hitch.will@gmail.com>
2020-08-29 09:31:48 -0700

    added trip jsx

--- client/src/pages/NotFound.js
+++ client/src/pages/NotFound.js
@@ -1,16 +1,16 @@
 // dependencies
-import React from 'react'
+import React from "react";
 
 const NotFound = () => {
-    return (
-        <div className="container" id="not-found">
-            <div className="row">
-                <div className="col s12">
-                    <span>404 Not Found</span>
-                </div>
-            </div>
+  return (
+    <div className="container" id="not-found">
+      <div className="row">
+        <div className="col s12">
+          <span>404 Not Found</span>
         </div>
-    )
-}
+      </div>
+    </div>
+  );
+};
 
-export default NotFound
+export default NotFound;
--- client/src/pages/Trip.js
+++ client/src/pages/Trip.js
@@ -9,13 +9,47 @@ const Trip = () => {
       <div className="row">
         <ul
           className="sidenav sidenav-fixed collection with-header"
-          style={{ zIndex: "0", height: "auto", marginTop: "64px", paddingBottom: "0" }}
+          style={{
+            zIndex: "0",
+            height: "auto",
+            marginTop: "64px",
+            paddingBottom: "0",
+          }}
         >
-        <li className="collection-header"><h4>Trips<span style={{float: "right"}}><i className="material-icons" style={{fontSize: "3rem"}}>landscape</i></span></h4></li>
-        <a href="#!" title="View Chart" className="collection-item">Australia<span style={{float: "right"}}><i className="material-icons" >show_chart</i></span></a>
-        <a href="#!" title="View Chart" className="collection-item">Poland<span style={{float: "right"}}><i className="material-icons" >show_chart</i></span></a>
-        <a href="#!" title="View Chart" className="collection-item">Costa Rica<span style={{float: "right"}}><i className="material-icons" >show_chart</i></span></a>
-        <a href="#!" title="View Chart" className="collection-item">India<span style={{float: "right"}}><i className="material-icons" >show_chart</i></span></a>
+          <li className="collection-header">
+            <h4>
+              Trips
+              <span style={{ float: "right" }}>
+                <i className="material-icons" style={{ fontSize: "3rem" }}>
+                  landscape
+                </i>
+              </span>
+            </h4>
+          </li>
+          <a href="#!" title="View Chart" className="collection-item">
+            Australia
+            <span style={{ float: "right" }}>
+              <i className="material-icons">show_chart</i>
+            </span>
+          </a>
+          <a href="#!" title="View Chart" className="collection-item">
+            Poland
+            <span style={{ float: "right" }}>
+              <i className="material-icons">show_chart</i>
+            </span>
+          </a>
+          <a href="#!" title="View Chart" className="collection-item">
+            Costa Rica
+            <span style={{ float: "right" }}>
+              <i className="material-icons">show_chart</i>
+            </span>
+          </a>
+          <a href="#!" title="View Chart" className="collection-item">
+            India
+            <span style={{ float: "right" }}>
+              <i className="material-icons">show_chart</i>
+            </span>
+          </a>
         </ul>
       </div>
     </div>
