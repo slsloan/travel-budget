4e2e1aa7f46f8ee9097a781e8ea1f0484a820370
willhitch <hitch.will@gmail.com>
2020-09-02 20:31:45 -0700

    added chart.js integration to TripDetail Componenent

--- client/src/components/TripDetails.js
+++ client/src/components/TripDetails.js
@@ -1,10 +1,22 @@
 import React from "react";
+import { Doughnut } from "react-chartjs-2";
 
 const TripDetails = ({ location, details, image, link }) => {
+  const data = {
+    labels: ["Room and Board", "Food", "Travel", "Misc"],
+    datasets: [
+      {
+        label: `${location} Budget`,
+        data: [600, 500, 1200, 200],
+        backgroundColor: ["#134a63", "#31708E", "#5085A5", "#8FC1E3"],
+      },
+    ],
+  };
   return (
     <div class="card-panel">
-      <img src={image} alt={details} width="100" hight="75"/>
-      <h3>{location}</h3>
+      <img src={image} alt={details} width="100" hight="75" />
+      <p>{location}</p>
+      <Doughnut data={data} />
       <p>{details}</p>
       <button href={link}>Learn More</button>
     </div>
