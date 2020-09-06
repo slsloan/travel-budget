d5ab8bab21876c712e226c8920498904ba73a8c7
Sammy Sloan <smileysammy3@gmail.com>
2020-09-06 14:40:25 -0600

    updated budget form

--- client/src/components/Budget.js
+++ client/src/components/Budget.js
@@ -1,7 +1,52 @@
 import React from "react";
 
 const Budget = () => {
-  return <p>Hello Budget!</p>;
+  return (
+    <div>
+      <br />
+      <div className="z-depth-1 row trip-page-element trip-card">
+        <center>
+          <i class="medium material-icons trip-header">public</i>
+          <h5 className=" trip-header">Create a Trip</h5>
+        </center>
+
+        <form className="col s12 login-input">
+          <div className="row">
+            <div className="col s12">
+              <input
+                className="validate"
+                type="text"
+                name="tripName"
+                id="tripName"
+                placeholder="Enter a name for your trip"
+              />
+              <label type="tripName"></label>
+            </div>
+          </div>
+
+          <div className="row login-input">
+            <div className="col s2 ">
+              <input
+                className="validate"
+                type="text"
+                name="lengthOfTrip"
+                id="lengthOfTrip"
+                placeholder="ex. 8"
+              />
+              <label type="lengthOfTrip"></label>
+            </div>
+            <h6 className="trip-header">days</h6>
+          </div>
+
+          <center>
+            <div className="row">
+              <a className="waves-effect waves-light btn-large trip-button"><i className="material-icons right">add</i>Add Trip</a>
+            </div>
+          </center>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default Budget;
