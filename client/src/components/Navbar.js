edbdf5c371ec7b4e87e49dac9b7ea44731b66751
willhitch <hitch.will@gmail.com>
2020-09-07 17:55:24 -0700

    removed class and added className

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,31 +1,31 @@
 // dependencies
 import React from "react";
-import { motion } from "framer-motion"
+import { motion } from "framer-motion";
 import { Link, NavLink } from "react-router-dom";
 
 // used to animate the logo svg
 const svgVariants = {
-  hidden: {rotate: -180 },
+  hidden: { rotate: -180 },
   visible: {
     rotate: 0,
-    transition: {duration: 1}
-  }
-}
+    transition: { duration: 1 },
+  },
+};
 
 const pathVariants = {
   hidden: {
     opacity: 0,
-    pathLength: 0
+    pathLength: 0,
   },
   visible: {
     opacity: 1,
     pathLength: 1,
     transition: {
       duration: 2,
-      ease: "easeInOut"
-    }
-  }
-}
+      ease: "easeInOut",
+    },
+  },
+};
 
 const Navbar = (props) => {
   const user = JSON.stringify(props.user).slice(1, -1);
@@ -36,7 +36,13 @@ const Navbar = (props) => {
         <div className="row">
           <div className="col s12">
             <Link to="/" className="brand-logo">
-            <motion.i class="huge material-icons" intitial="hidden" animate="visible">filter_hdr</motion.i>
+              <motion.i
+                className="huge material-icons"
+                intitial="hidden"
+                animate="visible"
+              >
+                filter_hdr
+              </motion.i>
               {user ? `${user}'s Travel Budget` : "Travel Budget"}
             </Link>
 
